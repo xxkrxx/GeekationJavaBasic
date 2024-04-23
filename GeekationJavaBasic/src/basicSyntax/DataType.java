d59c7ddcfe15a01b34175d045e04b1ca5edc4cdf
xxkrxx <ryoga3588@icloud.com>
2024-04-23 15:55:03 +0900

    修正

--- GeekationJavaBasic/src/basicSyntax/DataType.java
+++ GeekationJavaBasic/src/basicSyntax/DataType.java
@@ -2,12 +2,11 @@ package basicSyntax;
 
 public class DataType {
 
-	public static void main(String[] args) {
-		// 3つの文字列を連結
-		System.out.println("こんにちは" + "私は" + "Geek君です。");
-	    System.out.println(10);
-	    System.out.println("20"+"24");
-	    System.out.println(3.14);
-	    System.out.println(true);
-	}
+    public static void main(String[] args) {
+        System.out.println("こんにちは" + "私は" + "Geek君です。");
+        System.out.println(10);
+        System.out.println("20" + "24");
+        System.out.println(3.14);
+        System.out.println(true);
+    }
 }
