a2c11fe7569e8fb35846e77a2c5d698ed058bba9
xxkrxx <ryoga3588@icloud.com>
2024-04-24 11:47:31 +0900

    繰り返し処理課題

---
+++ GeekationJavaBasic/src/basicSyntax/ForWhile.java
@@ -0,0 +1,33 @@
+package basicSyntax;
+
+public class ForWhile {
+    public static void main(String[] args) {
+        // 1〜100までの数字を順にコンソールに出力してください。ただし、下記の条件を満たしてください。
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        String array[] = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "javascript"};
+        //繰り返し処理を行って、条件によって処理
+        //equalsメソッドとは、文字列の比較を行うことができる命令文です。
+        for (String language : array) {
+            if (language.equals("Java")) {
+                System.out.println("現在学習中の言語はJavaです。");
+                continue;
+            } else if (language.equals("HTML")) {
+                System.out.println("HTMLはプログラミング言語ではありません。");
+                continue;
+            } else {
+                System.out.println("対象のプログラミング言語");
+            }
+        }
+    }
+}
